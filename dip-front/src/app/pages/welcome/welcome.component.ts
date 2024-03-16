3b8b7355742db30dfda611c05801f6301230a4cf
sagyndykovaaida <alievnasg@gmail.com>
2024-03-16 12:17:41 +0500

    add welcome page

--- dip-front/src/app/pages/welcome/welcome.component.ts
+++ dip-front/src/app/pages/welcome/welcome.component.ts
@@ -1,8 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NzContentComponent, NzFooterComponent, NzHeaderComponent, NzLayoutComponent} from "ng-zorro-antd/layout";
+import {NzBreadCrumbComponent} from "ng-zorro-antd/breadcrumb";
+import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import {NzDividerComponent} from "ng-zorro-antd/divider";
+import {NzListComponent, NzListItemComponent} from "ng-zorro-antd/list";
+import {NzInputDirective} from "ng-zorro-antd/input";
 @Component({
   selector: 'app-welcome',
   standalone: true,
+  imports: [
+    NzLayoutComponent,
+    NzHeaderComponent,
+    NzContentComponent,
+    NzBreadCrumbComponent,
+    NzFooterComponent,
+    NzColDirective,
+    NzMenuModule,
+    NzDividerComponent,
+    NzRowDirective,
+    NzListComponent,
+    NzListItemComponent,
+    NzInputDirective
+  ],
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
